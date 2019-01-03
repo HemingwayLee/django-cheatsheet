66dff8a87e44c2656a27265bc20933b5f9217928
LeeHemingway <coherence0815@gmail.com>
2019-01-03 15:22:16 +0900

    add more template examples

--- MyTemplate/MyTemplate/urls.py
+++ MyTemplate/MyTemplate/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
     path('context/', views.hello_context),
+    path('more/', views.more_template),
 ]
--- MyTemplate/myapp/views.py
+++ MyTemplate/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def hello(request):
     return render(request, 'helloworld.html')
@@ -6,3 +7,8 @@ def hello(request):
 def hello_context(request):
     ct = { "age": 33 }
     return render(request, 'hellocontext.html', ct)
+
+def more_template(request):
+    users = User.objects.filter(is_staff=True).values()
+    ct = { "users": users }
+    return render(request, 'hellomore.html', ct)
