55b8a496372d406dae452f534c7c4d604f7c1319
LeeHemingway <coherence0815@gmail.com>
2019-01-03 15:04:43 +0900

    use context

--- MyTemplate/MyTemplate/urls.py
+++ MyTemplate/MyTemplate/urls.py
@@ -20,4 +20,5 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
+    path('context/', views.hello_context),
 ]
--- MyTemplate/myapp/views.py
+++ MyTemplate/myapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def hello(request):
     return render(request, 'helloworld.html')
+
+def hello_context(request):
+    ct = { "age": 33 }
+    return render(request, 'hellocontext.html', ct)
