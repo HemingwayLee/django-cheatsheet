84c4f5d157c3b465afdf53353e5344e6cf967fe3
LeeHemingway <coherence0815@gmail.com>
2019-01-13 12:15:57 +0900

    pass context

--- myjquery/myapp/views.py
+++ myjquery/myapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"address": "127.0.0.1"})
 
 def hello(request):
     return JsonResponse(json.loads('{"abc": 123}'), status=200)
