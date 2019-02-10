d072ee388f30be1aa86a720942d1536f78eabb82
LeeHemingway <coherence0815@gmail.com>
2019-02-10 10:54:31 +0900

    Init project

---
+++ testBulkInsert/main/views.py
@@ -0,0 +1,9 @@
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
+from django.http import HttpResponse
+
+@require_http_methods(["GET"])
+def hello(request):
+
+
+    return HttpResponse("hello")
