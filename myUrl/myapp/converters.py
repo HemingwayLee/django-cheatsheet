549f691cb0ad9b6d042b436b077d1622a84e516c
LeeHemingway <coherence0815@gmail.com>
2019-03-12 12:13:20 +0900

    fix indent

--- myUrl/myapp/converters.py
+++ myUrl/myapp/converters.py
@@ -1,9 +1,9 @@
 class ThreeDigitConverter:
-  regex = '[0-9]{3}'
+    regex = '[0-9]{3}'
 
-  # we must provide the following function
-  def to_python(self, value):
-    return int(value)
+    # we must provide the following function
+    def to_python(self, value):
+        return int(value)
 
-  def to_url(self, value):
-    return '%03d' % value
+    def to_url(self, value):
+        return '%03d' % value
--- myUrl/myapp/urls.py
+++ myUrl/myapp/urls.py
@@ -4,10 +4,10 @@ from . import views, converters
 register_converter(converters.ThreeDigitConverter, 'ddd')
 
 urlpatterns = [
-  path('', views.hello),
-  # path converter
-  path('integer/<int:myid>/', views.integer),
-  path('mul/<int:myid>/<str:name>', views.multiple),
-  # custom path converter
-  path('custom/<ddd:num>/', views.three),
+    path('', views.hello),
+    # path converter
+    path('integer/<int:myid>/', views.integer),
+    path('mul/<int:myid>/<str:name>', views.multiple),
+    # custom path converter
+    path('custom/<ddd:num>/', views.three),
 ]
