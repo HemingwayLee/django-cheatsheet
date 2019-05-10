bf8d1b59bb165658599435be1177dcc4a91d0c26
LeeHemingway <coherence0815@gmail.com>
2019-05-10 17:52:12 +0900

    add cookie based auth example

---
+++ auth/cookieBasedAuth/main/views.py
@@ -0,0 +1,35 @@
+import traceback
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["GET"])
+@login_required(login_url='/signin/')
+def hello(request):
+    return render(request, "hello.html")
+
+@require_http_methods(["GET"])
+def signin(request):
+    return render(request, "signin.html")
+
+@require_http_methods(["POST"])
+def dosignin(request):
+    try:
+        user = authenticate(username=request.POST["account"], password=request.POST["password"])
+        if not user:
+            return HttpResponse(status=404)
+
+        login(request, user)
+        return HttpResponse(status=200)
+    except:
+        print(traceback.format_exc())
+        return HttpResponse(status=500)
+
+@require_http_methods(["POST"])
+def signout(request):
+    logout(request)
+    return HttpResponse(status=200)
+
