dce5212428892b40da2fd8547883067a947bb7f4
LeeHemingway <coherence0815@gmail.com>
2021-06-30 17:25:50 +0900

    move superuser to env variable

--- auth/cms/scripts/create_superuser.sh
+++ auth/cms/scripts/create_superuser.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-USER="ywlee"
-PASS="pwd"
 MAIL="ywlee@gmail.com"
 SCRIPT="
 from django.contrib.auth.models import User
-username = '$USER'
-password = '$PASS'
+username = '$DJANGO_USER'
+password = '$DJANGO_PASS'
 email = '$MAIL'
 if User.objects.filter(username=username).count()==0:
   User.objects.create_superuser(username, email, password)
