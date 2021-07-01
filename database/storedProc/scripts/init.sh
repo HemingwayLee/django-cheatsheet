64985b1f42ab39899aa625d1708ccebdd56f41f9
LeeHemingway <coherence0815@gmail.com>
2021-07-01 09:46:42 +0900

    add hotfix mode

--- database/storedProc/scripts/init.sh
+++ database/storedProc/scripts/init.sh
@@ -14,12 +14,19 @@ cd /home/proj/
 python3 manage.py makemigrations myapp
 python3 manage.py migrate
 
-cd /home/proj/scripts/
-./create_superuser.sh
+COUNT=$(PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -d ${POSTGRES_DB_NAME} -tAc 'select count(*) from auth_user;')
+if [ "$COUNT" -gt "0" ] ; then
+  >&2 echo "Superuser Exist!!! This is hotfix mode"
+  >&2 echo "No need to create superuser"
+else
+  >&2 echo "Superuser NOT Exist!!! This is fresh install mode"
+  cd /home/proj/scripts/
+  ./create_superuser.sh
+  cd /home/proj/sql/
+  PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -d ${POSTGRES_DB_NAME} -a -f func.sql
+  PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -d ${POSTGRES_DB_NAME} -a -f my_view.sql
+fi
 
-cd /home/proj/sql/
-PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -d ${POSTGRES_DB_NAME} -a -f func.sql
-PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -d ${POSTGRES_DB_NAME} -a -f my_view.sql
 
 cd /home/proj/
 python3 manage.py runserver 0.0.0.0:8000
