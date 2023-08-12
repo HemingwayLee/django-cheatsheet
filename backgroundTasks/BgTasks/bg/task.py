6f232ae10a39a9b81d31c2cd7bdf737648c3fd7c
HemingwayLee <coherence0815@gmail.com>
2023-08-12 11:06:10 +0900

    update indent

--- backgroundTasks/BgTasks/bg/task.py
+++ backgroundTasks/BgTasks/bg/task.py
@@ -2,4 +2,4 @@ from background_task import background
 
 @background(schedule=10)
 def demo_task(message):
-  print("in the background...".format(message))
+    print("in the background...".format(message))
--- backgroundTasks/BgTasks/bg/views.py
+++ backgroundTasks/BgTasks/bg/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from . import task
 
 def callme(request):
-  task.demo_task('hello, bg')
-  return HttpResponse("Hello, check your console")
+    task.demo_task('hello, bg')
+    return HttpResponse("Hello, check your console")
